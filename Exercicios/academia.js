06eb2d2bcd9a3d981e05ba9e426f29454af8f7bb
aulas-programacao@bol.com.br <luiz.fernando9@senacsp.edu.br>
2024-09-19 22:29:36 -0300

    estudos das variaveis

---
+++ Exercicios/academia.js
@@ -0,0 +1,30 @@
+/**
+ * Ficha de um aluno de academia    
+ * @autor Luiz Fernando
+ */
+
+//variaveis
+let nome
+let idade
+let peso 
+let altura
+let vip
+
+console.clear()
+
+//entrada de dados
+nome = "Luiz Fernando"
+idade = 44
+peso = 76
+altura = 1.83
+vip = true
+
+//saida
+console.log("ficha do aluno")
+console.log("____________") 
+console.log(`nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`VIP: ${vip}`)
+
